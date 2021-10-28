36b405934ca3edb97f480224fa11d45884b13eef
fridahnamudu <fridah.namudu@gmail.com>
2021-10-28 23:55:02 +0300

    add changes

--- chat.py
+++ chat.py
@@ -24,33 +24,33 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Frida"
-print("Let's chat! type 'quit' to exit")
-while True:
-    sentence = input('You: ')
-    if sentence == "quit":
-        break
-    # tokenizing our sentence
-    sentence = tokenize(sentence)
-    # creating the bag of words
-    X = bag_of_words(sentence, all_words)
-    # reshaping
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X)
-
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
-    tag = tags[predicted.item()]
-    # checking the probability
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-
-    if prob.item() > 0.75:
-        for intent in intents["intents"]:
-            if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand...")
+# bot_name = "Frida"
+# print("Let's chat! type 'quit' to exit")
+# while True:
+#     sentence = input('You: ')
+#     if sentence == "quit":
+#         break
+#     # tokenizing our sentence
+#     sentence = tokenize(sentence)
+#     # creating the bag of words
+#     X = bag_of_words(sentence, all_words)
+#     # reshaping
+#     X = X.reshape(1, X.shape[0])
+#     X = torch.from_numpy(X)
+#
+#     output = model(X)
+#     _, predicted = torch.max(output, dim=1)
+#     tag = tags[predicted.item()]
+#     # checking the probability
+#     probs = torch.softmax(output, dim=1)
+#     prob = probs[0][predicted.item()]
+#
+#     if prob.item() > 0.75:
+#         for intent in intents["intents"]:
+#             if tag == intent["tag"]:
+#                 print(f"{bot_name}: {random.choice(intent['responses'])}")
+#     else:
+#         print(f"{bot_name}: I do not understand...")
 
 
 def get_response(text):
